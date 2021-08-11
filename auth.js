b4dcabf2d3816c945a4c2f0e6ecfa8ae56f34b5f
unknown <talha.bayansar@hotmail.com>
2021-08-11 12:46:53 +0200

    shopwedo-api done

--- auth.js
+++ auth.js
@@ -1,37 +1,40 @@
-const crypto = require('crypto-js');
+const crypto = require("crypto-js");
 
 class Auth {
-    constructor(shopId, shopKey) {
-        this.shopId = shopId,
-        this.shopKey = shopKey
+  #shopId;
+  #shopKey;
+  constructor(shopId, shopKey) {
+    (this.#shopId = shopId), (this.#shopKey = shopKey);
+  }
+
+  // Getters
+  get authJson() {
+    return this.generateAuthJson();
+  }
+
+  //Methods
+  generateAuthJson() {
+    Date.prototype.getUnixTime = function () {
+      return (this.getTime() / 1000) | 0;
     };
 
-    // Getters
-    get authJson() {
-        return this.generateAuthJson();
-    }
+    const shopid = this.#shopId;
+    const shopkey = this.#shopKey;
+    const timestamp = new Date().getTime() / 1000;
+    const salt = "dfvoijerhiugvezhgiuezrhg"; //crypto.lib.WordArray.random(128 / 8);
 
-    //Methods
-    generateAuthJson() {
-        Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
+    //A unique hash HMAC SHA-512 encrypted string of the concatenation of the shop ID, shop KEY, timestamp and salt.
+    const tokenInput = `${shopid}${shopkey}${timestamp}${salt}`;
 
-        const shopid = this.shopId;
-        const shopkey = this.shopKey;
-        const timestamp = ((new Date().getTime())/1000);
-        const salt = 'dfvoijerhiugvezhgiuezrhg'; //crypto.lib.WordArray.random(128 / 8);
-            
-        //A unique hash HMAC SHA-512 encrypted string of the concatenation of the shop ID, shop KEY, timestamp and salt.
-        const tokenInput = `${shopid}${shopkey}${timestamp}${salt}`;
+    const token = crypto.HmacSHA512(tokenInput, shopkey).toString();
 
-        const token = crypto.HmacSHA512(tokenInput, shopkey).toString();
-
-        return JSON.stringify({
-            "shopid": shopid,
-            "timestamp": timestamp,
-            "salt": salt,
-            "token": token
-        });
-    }
+    return JSON.stringify({
+      shopid: shopid,
+      timestamp: timestamp,
+      salt: salt,
+      token: token,
+    });
+  }
 }
 
 module.exports = Auth;
--- npm.js
+++ npm.js
@@ -1,37 +1,16 @@
-// const express = require("express");
 const Auth = require("./auth");
 const request = require("request");
-// const cors = require("cors");
-// require("dotenv").config();
 
-//CONSTANTS
-// const app = express();
-// const port = 3001;
-// const auth = new Auth(process.env.SHOP_ID, process.env.SHOP_KEY);
-
-//MIDDLEWARE
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
-
-class Package {
-  #shopId;
-  #shopKey;
-
+class ShopWeDoApi {
   constructor(shopId, shopKey) {
-    this.#shopId = shopId;
-    this.#shopKey = shopKey;
     this.auth = new Auth(shopId, shopKey);
   }
 
   //methods
-  logParams() {
-    console.log(`shopId = ${this.#shopId}\nshopKey = ${this.#shopKey}`);
-  }
 
-  request(endpoint, data, method, callback) {
+  post(endpoint, data, callback) {
     const options = {
-      method: method || "POST",
+      method: "POST",
       url: `https://admin.shopwedo.com/api/${endpoint}`,
       formData: {
         auth: this.auth.authJson,
@@ -43,6 +22,17 @@ class Package {
       callback(error, body);
     });
   }
+
+  get(endpoint, callback) {
+    const options = {
+      method: "GET",
+      url: `https://admin.shopwedo.com/api/${endpoint}`,
+    };
+
+    request(options, (error, response, body) => {
+      callback(error, body);
+    });
+  }
 }
 
-module.exports = Package;
+module.exports = ShopWeDoApi;
