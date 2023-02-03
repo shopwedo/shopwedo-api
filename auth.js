ee02d875ee89d8dc1b127d4e394795e94fe92cdb
shopwedo <88786242+shopwedo@users.noreply.github.com>
2023-02-03 14:35:26 +0100

    update package name

--- auth.js
+++ auth.js
@@ -1,40 +1,38 @@
-const crypto = require("crypto-js");
+const Auth = require("./auth");
+const request = require("request");
 
-class Auth {
-  #shopId;
-  #shopKey;
+class ShopWeDoApi {
   constructor(shopId, shopKey) {
-    (this.#shopId = shopId), (this.#shopKey = shopKey);
+    this.auth = new Auth(shopId, shopKey);
   }
 
-  // Getters
-  get authJson() {
-    return this.generateAuthJson();
-  }
+  //methods
 
-  //Methods
-  generateAuthJson() {
-    Date.prototype.getUnixTime = function () {
-      return (this.getTime() / 1000) | 0;
+  post(endpoint, data, callback) {
+    const options = {
+      method: "POST",
+      url: `https://admin.shopwedo.com/api/${endpoint}`,
+      formData: {
+        auth: this.auth.authJson,
+        data: data,
+      },
     };
 
-    const shopid = this.#shopId;
-    const shopkey = this.#shopKey;
-    const timestamp = new Date().getTime() / 1000;
-    const salt = "dfvoijerhiugvezhgiuezrhg"; //crypto.lib.WordArray.random(128 / 8);
+    request(options, (error, response, body) => {
+      callback(error, body);
+    });
+  }
 
-    //A unique hash HMAC SHA-512 encrypted string of the concatenation of the shop ID, shop KEY, timestamp and salt.
-    const tokenInput = `${shopid}${shopkey}${timestamp}${salt}`;
+  get(endpoint, callback) {
+    const options = {
+      method: "GET",
+      url: `https://admin.shopwedo.com/api/${endpoint}`,
+    };
 
-    const token = crypto.HmacSHA512(tokenInput, shopkey).toString();
-
-    return JSON.stringify({
-      shopid: shopid,
-      timestamp: timestamp,
-      salt: salt,
-      token: token,
+    request(options, (error, response, body) => {
+      callback(error, body);
     });
   }
 }
 
-module.exports = Auth;
+module.exports = ShopWeDoApi;
